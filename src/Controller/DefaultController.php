5f97056c9466984d8295c4abad304327c708db63
Pierre Bretéché <pbreteche@dawan.fr>
2021-09-20 15:32:47 +0200

    Manipulation objet Response

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,13 +8,25 @@ class DefaultController
 {
     public function homepage(): Response
     {
-        return new Response(
+        $response = new Response(
             'Ceci est le corps de ma réponse HTTP',
             Response::HTTP_OK,
             [
                 'Content-type' => 'text/plain',
             ]
         );
+
+        // Toute manipulation de la réponse HTTP doit se faire
+        // au travers de cet objet "response"
+        // Ceci implique l'interdiction formelle d'utiliser:
+        // echo, print, print_r, var_dump, header, http_response_code
+        $response
+            ->setContent('contenu écrasant le précédent')
+            ->setCharset('utf8')
+            ->headers->set('Content-type', 'text/html')
+        ;
+
+        return $response;
     }
 
     public function hello(string $name): Response
