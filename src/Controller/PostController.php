691f60b67d1c5ba20e4ed36549d3f92918c0a7f4
Pierre Bretéché <pbreteche@dawan.fr>
2021-09-21 12:23:40 +0200

    Liaison complète: route -> controller -> template

---
+++ src/Controller/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/post", methods="GET")
+     */
+    public function index(): Response
+    {
+        $post = (new Post())
+            ->setTitle('Un titre exemple')
+            ->setBody('Le contenu de la publication serait ici.')
+            ->setCreatedAt(new \DateTimeImmutable('last monday 09:45:56'))
+            ->setIsPublished(true)
+        ;
+
+        return $this->render('post/index.html.twig', [
+            'post' => $post,
+        ]);
+    }
+}
